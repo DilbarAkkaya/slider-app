ff140825e81d2eae7be808430910745761e5c721
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-10-31 10:15:03 +0300

    fix: fix mistake with parens

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import {Component, useState} from 'react';
-import {Container} from 'react-bootstrap';
+import { useState } from 'react';
+import { Container } from 'react-bootstrap';
 import './App.css';
 
- class Slider extends Component {
+/*  class Slider extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -35,35 +35,39 @@ import './App.css';
       </Container>
     )
 }
-} 
+}  */
 
-/* const Slider = (props)=> {
-    const {slide, setSlide} = useState(0);
-    const {autoplay, setAutoplay} = useState(false);
-    function changeSlide(i) {
-      setSlide(slide=> slide + i);
-    }
-function toggleAutoplay (){
-  setAutoplay(!autoplay);
-}
-    return (
-      <Container>
-        <div className='slider w-50 m-auto'>
-          <img className='d-block w-100' src="http://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
-          <div className="text-center mt-5">Active slide {slide}<br/> {autoplay ? 'auto' : null}</div>
-          <div className="buttons mt-3">
-            <button className="btn btn-primary me-2" onClick={() => changeSlide(-1)}>-1</button>
-            <button className="btn btn-primary me-2" onClick={() => changeSlide(1)}>+1</button>
-            <button className="btn btn-primary me-2" onClick={toggleAutoplay}>toggle Autoplay</button>
-          </div>
-        </div>
-      </Container>
-    )
-}
- */
-function App () {
+const Slider = (props) => {
+
+  const [ slide, setSlide ] = useState(0);
+  const [ autoplay, setAutoplay ] = useState(false);
+
+  function changeSlide(i) {
+    setSlide(slide => slide + i);
+  }
+
+  function toggleAutoplay() {
+    setAutoplay(!autoplay);
+  }
+
   return (
-    <Slider/>
+    <Container>
+      <div className='slider w-50 m-auto'>
+        <img className='d-block w-100' src="http://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+        <div className="text-center mt-5">Active slide {slide} <br/> {autoplay ? 'auto' : null}</div>
+        <div className="buttons mt-3">
+          <button className="btn btn-primary me-2" onClick={() => changeSlide(-1)}>-1</button>
+          <button className="btn btn-primary me-2" onClick={() => changeSlide(1)}>+1</button>
+          <button className="btn btn-primary me-2" onClick={toggleAutoplay}>toggle Autoplay</button>
+        </div>
+      </div>
+    </Container>
+  )
+}
+
+function App() {
+  return (
+    <Slider />
   )
 }
 export default App;
